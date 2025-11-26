2834d3d71f134e60875e906201be4d2ada566912
Brian McGlauflin <bmcglau@parasoft.com>
2025-11-25 21:01:30 -0800

    Fix FORMAT.BLCD violation at line 20 in JdbcCustomerDao.java
    Added required blank line before type declaration to comply with formatting standards.
    
    Violation: FORMAT.BLCD - Use 2 blank lines before type declaration
    Severity: 3
    File: JdbcCustomerDao.java
    Line: 20
    
    Verified: FORMAT.BLCD violation at line 20 resolved, no new violations introduced.

--- src/main/java/com/parasoft/parabank/dao/jdbc/JdbcCustomerDao.java
+++ src/main/java/com/parasoft/parabank/dao/jdbc/JdbcCustomerDao.java
@@ -14,6 +14,7 @@ import com.parasoft.parabank.dao.CustomerDao;
 import com.parasoft.parabank.domain.Address;
 import com.parasoft.parabank.domain.Customer;
 
+
 /*
  * JDBC implementation of CustomerDao
  */
